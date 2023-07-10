763854817799e9f98d6766e4b494422f8e45bb8d
Hamza <hamzayasin999@gmail.com>
2023-07-10 22:28:45 +0500

    update string descriptions

--- Sources/Animator/ESRefreshHeaderAnimator.swift
+++ Sources/Animator/ESRefreshHeaderAnimator.swift
@@ -30,14 +30,14 @@ import UIKit
 
 @available(iOS 13.0, *)
 open class ESRefreshHeaderAnimator: UIView, ESRefreshProtocol, ESRefreshAnimatorProtocol, ESRefreshImpactProtocol {
-    open var pullToRefreshDescription = NSLocalizedString("Pull to refresh          ", comment: "") {
+    open var pullToRefreshDescription = NSLocalizedString("Pull to refresh       ", comment: "") {
         didSet {
             if pullToRefreshDescription != oldValue {
                 titleLabel.text = pullToRefreshDescription;
             }
         }
     }
-    open var releaseToRefreshDescription = NSLocalizedString("Release to refresh            ", comment: "")
+    open var releaseToRefreshDescription = NSLocalizedString("Release to refresh         s", comment: "")
     open var loadingDescription = NSLocalizedString("checking for more 🗞...", comment: "")
 
     open var view: UIView { return self }
